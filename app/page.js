6f896a547d1c01ea67b1db89e86d0c58f47f8083
Hazemyahea <hazemyahea1@gmail.com>
2024-07-25 00:54:56 +0300

    edite

--- app/page.js
+++ app/page.js
@@ -4,24 +4,24 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className=" container mx-auto p-5">
-      <h2 className=" text-3xl font-semibold">Hi there, I'm Hazem! 👋</h2>
+      <h2 className=" text-3xl font-semibold">{`Hi there, I'm Hazem! 👋`}</h2>
       <p className="p-3 mt-1">
-        I'm a passionate software engineer specializing in front-end development
+        {` I'm a passionate software engineer specializing in front-end development
         with a focus on React and Next.js. I love crafting beautiful and
-        functional user interfaces that enhance user experiences.
+        functional user interfaces that enhance user experiences.`}
       </p>
       <ul className=" flex flex-col gap-3  p-4 border-t-2">
         <li>
-          🔭 I’m currently working on honing my skills in React and exploring
-          server-side rendering with Next.js.
+          {`🔭 I’m currently working on honing my skills in React and exploring
+          server-side rendering with Next.js.`}
         </li>
         <li>
-          🌱 I’m continuously learning and staying updated with the latest
-          trends in web development.
+          {` 🌱 I’m continuously learning and staying updated with the latest
+          trends in web development.`}
         </li>
         <li>
-          💬 Ask me about front-end development, React, or anything related to
-          web technologies!
+          {`💬 Ask me about front-end development, React, or anything related to
+          web technologies!`}
         </li>
       </ul>
       <div className="mt-5">
@@ -33,6 +33,7 @@ export default function Home() {
                 width={40}
                 height={40}
                 loading="lazy"
+                alt="Linkedin"
               />
             </li>
           </a>
@@ -43,6 +44,7 @@ export default function Home() {
                 width={40}
                 height={40}
                 loading="lazy"
+                alt="Soical"
               />
             </li>
           </a>
@@ -56,6 +58,7 @@ export default function Home() {
                 width={40}
                 height={40}
                 loading="lazy"
+                alt="My Cv"
               />
             </li>
           </a>
